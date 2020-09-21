6dc288519526975f5f4bf73beae8bc309fdf525f
artemya <artemya@tahometer.com>
2020-09-21 11:32:46 +0300

    Create header

---
+++ src/components/shared/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success ">
+        <a className="home" href="/">Home</a>
+
+        <ul className="nav navbar-nav ml-auto white">
+            <li className="nav-item">
+                <a href="/api/services" className="header-links">Services</a>
+            </li>
+            <li className="nav-item">
+                <a href="/api/materials" className="header-links">Materials</a>
+            </li>
+        </ul>
+    </nav>
+    )
+}
